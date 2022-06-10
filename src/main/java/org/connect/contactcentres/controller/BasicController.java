18efe371598cec71850c81bd654056b775b92584
Maxp73 <massimo_sposato@hotmail.com>
2022-06-10 15:47:26 +0100

    removed space
    removed space

--- src/main/java/org/connect/contactcentres/controller/BasicController.java
+++ src/main/java/org/connect/contactcentres/controller/BasicController.java
@@ -12,7 +12,4 @@ public abstract class BasicController {
 	@Autowired
 	protected IFunctionalityService functionalityService;
 	
-	 
-	
-	
 }
--- src/main/java/org/connect/contactcentres/controller/ConfigurationController.java
+++ src/main/java/org/connect/contactcentres/controller/ConfigurationController.java
@@ -3,10 +3,7 @@ package org.connect.contactcentres.controller;
 import java.util.List;
 
 import org.connect.contactcentres.model.TransferLocations;
-import org.connect.contactcentres.service.IFunctionalityService;
-import org.connect.contactcentres.service.IServiceFactory;
 import org.connect.contactcentres.utility.ReadFileIO;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
